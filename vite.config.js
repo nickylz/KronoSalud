ea1ed57d0d30ff3c22a0fe7f81ce92c19361dc9d
nickylz <zanadrianzenbohorquez@crackthecode.la>
2026-08-17 20:06:58 -0500

    deploy

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite' 
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/hp-we/', // Asegúrate de que coincida exactamente con el nombre de tu repositorio
+  base: '/KronoSalud/', // Nombre exacto de tu repositorio
   plugins: [
     react(),
     tailwindcss(),
